757180afcee3c56cc492479629367599f69d39c3
Usuario <alanfaraj@outlook.com>
2024-06-30 02:37:49 -0300

    Atualização do projeto deixando responsivo

--- Front/src/componentes/footer.jsx
+++ Front/src/componentes/footer.jsx
@@ -8,14 +8,14 @@ function Footer() {
     <footer className="bg-light text-white mt-5">
       <div className="container-fluid bg-light text-white py-5">
         <div className="row">
-          <div className="col-md-6">  
+          <div className="col-md-6  ">    
             <h5 className='colorName'>Troca Disco</h5>
-            <p className='colorParagrafo'>Seu site favorito para trocar  discos em geral.</p>
+            <p className='colorParagrafo'>Seu site favorito para trocar discos em geral.</p>
           </div>
 
           
-          <div className="col-md-3">
-            <h5 className='colorLinks'>Links Rápidos</h5>
+          <div className="col-md-3  ">
+            <h5 className='colorLinks '>Links Rápidos</h5>
             <ul className="list-unstyled">
               <li><a className="colorLinks2" href="#"style={{ textDecoration: 'none'}}>Início</a></li>
               <li><a className="colorLinks2" href="#"style={{ textDecoration: 'none'}}>Produtos</a></li>
@@ -23,8 +23,8 @@ function Footer() {
               <li><a className="colorLinks2" href="#"style={{ textDecoration: 'none'}}>Contato</a></li>
             </ul>
           </div>
-          <div className="col-md-3">
-            <h5 className='colorRedes'>Redes Sociais</h5>
+          <div className="col-md-3  ">
+            <h5 className='colorRedes '>Redes Sociais</h5>
             <ul className="list-unstyled ">
               <li><a className="colorRedes2" href="#"style={{ textDecoration: 'none'}}>Facebook</a></li>
               <li><a className="colorRedes2" href="#"style={{ textDecoration: 'none'}}>Twitter</a></li>
@@ -40,6 +40,8 @@ function Footer() {
     </footer>
   );
 }
+
+
 
 export default Footer;
 
